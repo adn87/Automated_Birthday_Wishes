2a1ad1401312b18a1807597de7188b7a041db7a9
adn87 <aaghadieh@gmail.com>
2023-11-13 10:51:39 -0500

    Automated_Birthday_Wishes_002
    - sending random motivational emails by combining the datetime and smtplib in python

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import smtplib
+import datetime as dt
+import random
 
-# enter your own email here
 my_email = ""
 password = ""
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="[enter the recipient's email here]",
-                        msg="Subject:Hello\n\nThis is the body of my email"
-                        )
+now = dt.datetime.now()
+weekday = now.weekday()
+# 0 is for monday, 1 for Tuesday, 2 for Wednesday, and so on...
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="[enter the recipient's email here]",
+                            msg=f"Subject:Did you get it\n\n{quote}")
+
